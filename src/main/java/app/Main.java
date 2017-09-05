f3b9045cb53b07aceebd9c457dd857a8cacd24bd
Jean Gonçalves <jean.gnc@gmail.com>
2017-09-05 13:50:04 -0300

    Singleton para gerenciar conexão com o banco

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,46 +1,20 @@
 package app;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import app.database.DatabaseConnection;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        testDatabase();
-    }
 
-    private static void testDatabase() {
-        Connection connection = null;
-        ResultSet resultSet = null;
-        Statement statement = null;
+        DatabaseConnection.getDefaultConnection();
+//            statement = connection.createStatement();
+//            resultSet = statement.executeQuery("SELECT teste FROM teste");
 
-        try {
-            connection = DriverManager.getConnection("jdbc:derby:./db;create=true", "", "");
-            Statement statement1 = connection.createStatement();
-            statement1.execute("CREATE TABLE teste (teste VARCHAR (255))");
+//            while (resultSet.next()) {
+//                System.out.println("COLUNA:" + resultSet.getString("teste"));
+//            }
 
-            Statement statement2 = connection.createStatement();
-            statement2.execute("INSERT INTO teste (teste) VALUES ('oh meu deus')");
-
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT teste FROM teste");
-
-            while (resultSet.next()) {
-                System.out.println("COLUNA:" + resultSet.getString("teste"));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                resultSet.close();
-                statement.close();
-                connection.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        DatabaseConnection.closeDefaultConnection();
     }
 }
---
+++ src/main/java/app/database/DatabaseConnection.java
@@ -0,0 +1,51 @@
+package app.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+    private static Connection c;
+
+    public static Connection getDefaultConnection()
+    {
+        if (c == null) {
+            try {
+                c = DriverManager.getConnection("jdbc:derby:./db;create=true", "", "");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            createDatabaseStructure();
+        }
+
+        return c;
+    }
+
+    /**
+     *
+     */
+    public static void closeDefaultConnection()
+    {
+        try {
+            c.close();
+            c = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     */
+    private static void createDatabaseStructure()
+    {
+        try {
+            Statement statement1 = c.createStatement();
+            statement1.execute("CREATE TABLE teste (teste VARCHAR (255))");
+        } catch (Exception e) {
+            //whatever
+        }
+    }
+}
